27ea535e9c445f3759e9677ec4d6e69345be7c47
aspsptyd <devadiroot@gmail.com>
2025-02-10 16:22:13 +0700

    Update: first program define file

--- Program.cs
+++ Program.cs
@@ -5,9 +5,5 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello World!");
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
-        var name = Console.ReadLine();
-        Console.WriteLine("Date now: " + DateTime.Now);
     }
 }

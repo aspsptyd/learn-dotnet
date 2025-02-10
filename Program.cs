084e4bed81ab37aa3d20a59cc6f11f2c1a27df49
aspsptyd <devadiroot@gmail.com>
2025-02-10 15:58:48 +0700

    First commit & first project .NET console for learn

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+        var name = Console.ReadLine();
+        Console.WriteLine("Date now: " + DateTime.Now);
+    }
+}
